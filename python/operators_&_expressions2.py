4e35e5a135c5f77fdfde40d4b1b6e7539867f9b8
oktvsm <oktavianuss.misro@gmail.com>
2022-10-08 14:44:26 +0700

    solution for scenario 2

--- python/operators_&_expressions2.py
+++ python/operators_&_expressions2.py
@@ -3,10 +3,17 @@
 # x = 1                     # y = 0.6000000000000001
 # x = 10                    # y = 0.09901951266867294
 
-x = float(input("Enter value for x: "))
+# x = float(input("Enter value for x: "))
 
-y = 1 / (x + (1 / (x + 1 / (x + 1/x))))
+# y = 1 / (x + (1 / (x + 1 / (x + 1/x))))
 
-print("y =", y)
+# print("y =", y)
 
-# still don't know the answer, need to be solved
+# scenario 2
+hour = int(input('Starting time (hours): '))
+minutes = int(input('Starting time (minutes): '))
+duration = int(input('Event duration (minutes): '))
+
+end_minutes = 60 - minutes
+end_hour = hour + 1
+print(end_hour,':',end_minutes)
