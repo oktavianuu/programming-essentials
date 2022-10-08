4ead9fe49e58e55c33c718292c474202ec11bea7
oktvsm <oktavianuss.misro@gmail.com>
2022-10-08 21:59:41 +0700

    add branching.py

---
+++ python/branching.py
@@ -0,0 +1,9 @@
+# get the number from user
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# choose larger number
+if number1 > number2: larger_number = number1 
+else: larger_number = number2
+
+print("The larger num975ber is: ", larger_number)
