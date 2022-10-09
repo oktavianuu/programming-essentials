2ab7e6db6ab4c5d50f3627ea80261c14a16e9359
oktvsm <oktavianuss.misro@gmail.com>
2022-10-09 11:49:43 +0700

    add solution to conditional_spathiphyllum.py

---
+++ python/conditional_spathiphyllum.py
@@ -0,0 +1,8 @@
+plant = input("Enter the best plant here: ")
+fav_plant = 'Spathiphyllum'
+if plant == fav_plant:
+    print(" Yes - Spathiphyllum is the best plant ever!")
+if plant == 'spathyphyllum':
+    print("No, I want a big Spathiphyllum!")
+else:
+    print(f"Spathiphyllum! Not {plant}!")
