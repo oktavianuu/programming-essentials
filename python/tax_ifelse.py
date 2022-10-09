0974c5e501af604faa980ac9e667c7df31b06fa7
oktvsm <oktavianuss.misro@gmail.com>
2022-10-09 14:16:34 +0700

    improve solution for tax_ifelse.py

--- python/tax_ifelse.py
+++ python/tax_ifelse.py
@@ -3,4 +3,8 @@ income = float(input("Enter the annual income: "))
 if income < 85528:
     tax = income * 0.18 - 556.02
 else:
-    tax = 14839 + 0.32 * 85528
+    tax_amount = income - 85528
+    tax = 14839.02 + 0.32 * tax_amount
+
+tax = round(tax, 0)
+print("The tax is", tax, "thalers.")
