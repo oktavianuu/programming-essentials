8bae3b3df38be1f618e743b9b19c23ac0eaaf517
oktvsm <oktavianuss.misro@gmail.com>
2022-10-10 22:22:58 +0700

    improve code in exit_loop_with_counter.py

--- python/exit_loop_with_counter.py
+++ python/exit_loop_with_counter.py
@@ -1,3 +1,4 @@
+from itertools import count
 import time
 counter = int(input("Enter your favorite number: "))
 
@@ -6,3 +7,14 @@ while counter != 0:
     counter -= 1
     time.sleep(0.5)
 print("Outside the loop.", counter)
+
+# compact version
+print("compact version of the previous code.")
+counter = int(input("Enter your favorite number: "))
+
+while counter:
+    print("Inside the loop.", counter)
+    counter -= 1
+    time.sleep(0.5)
+print("Outside the loop.", counter)
+print("Thanks buddy!")
