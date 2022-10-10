52e0cd402900a241880bd33ceca0f84a28fc98df
oktvsm <oktavianuss.misro@gmail.com>
2022-10-10 22:17:34 +0700

    modify exit_loop_with_counter.py

--- python/exit_loop_with_counter.py
+++ python/exit_loop_with_counter.py
@@ -1,6 +1,8 @@
+import time
 counter = int(input("Enter your favorite number: "))
 
 while counter != 0:
     print("Inside the loop.", counter)
     counter -= 1
+    time.sleep(0.5)
 print("Outside the loop.", counter)
