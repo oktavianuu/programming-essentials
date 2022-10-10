09620371dd21737cbe66b6fa65bc40edd015ef51
oktvsm <oktavianuss.misro@gmail.com>
2022-10-10 15:14:40 +0700

    improve loop.py

--- python/loop.py
+++ python/loop.py
@@ -1,7 +1,10 @@
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
-number3 = int(input("Enter the third number: "))
+largest_number = -999999999
 
-largest_number = max(number1, number2, number3)
+number = int(input("Enter a number or type -1 to stop: "))
+
+while number != -1:
+    if number > largest_number:
+        largest_number = number
+    number = int(input("Enter a number or type -1 to stop: "))
 
 print("The largest number is:", largest_number)
