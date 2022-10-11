31c6924089df69d2c3e134dd18d81a59381de783
oktvsm <oktavianuss.misro@gmail.com>
2022-10-11 14:50:38 +0700

    finalizing for_loop.py

--- for_loop.py
+++ for_loop.py
@@ -12,13 +12,22 @@ import time
 #    time.sleep(0.009)
 
 # for is designed to do more complicated tasks – it can "browse" large collections of data item by item. 
+print("range with one argument.")
 for i in range(10):
     print("The value of i is currently", i)
-    time.sleep(0.3)
-
-time.sleep(0.5)
-print("Done!")
-
 # range function may be equiped with two arguments not just one
-for i in range(2, 8): # start with two, end in 7
+print("range with two arguments.")
+for i in range(2, 8): # start with two, end in 75
     print("The value of i is currently", i)
+
+# range with three arguments
+print("range with three arguments.")
+for i in range(2, 8, 3): # the third arguments indicates the step
+    print("The value of i is currently", i)
+
+print("write some of the first powers of two.")
+power = 1
+for expo in range(16):
+    print("2 to the power of", expo, "is", power)
+    power *= 2
+    
