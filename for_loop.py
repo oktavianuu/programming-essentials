afa366dd943e313e26da2837ccd79cfa9b485486
oktvsm <oktavianuss.misro@gmail.com>
2022-10-11 14:36:30 +0700

    add more loop example

--- for_loop.py
+++ for_loop.py
@@ -7,6 +7,18 @@ import time
 #print("Yeayy!")
 
 # more compact using for loop
-for i in range(101):
-    print(i)
-    time.sleep(0.009)
+#for i in range(101):
+#    print(i)
+#    time.sleep(0.009)
+
+# for is designed to do more complicated tasks – it can "browse" large collections of data item by item. 
+for i in range(10):
+    print("The value of i is currently", i)
+    time.sleep(0.3)
+
+time.sleep(0.5)
+print("Done!")
+
+# range function may be equiped with two arguments not just one
+for i in range(2, 8): # start with two, end in 7
+    print("The value of i is currently", i)
