9705630269b0d339026efcf29ebb223a9c275653
oktvsm <oktavianuss.misro@gmail.com>
2022-10-11 14:20:22 +0700

    add for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+import time
+#i = 0 
+#while i < 100:
+#    i += 1
+#    time.sleep(0.01)
+#    print(i)
+#print("Yeayy!")
+
+# more compact using for loop
+for i in range(101):
+    print(i)
+    time.sleep(0.009)
