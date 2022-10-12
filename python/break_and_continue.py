7b096ca9507d8152b77fcfb4c3aa9b11216e039e
oktvsm <oktavianuss.misro@gmail.com>
2022-10-12 20:21:25 +0700

    resolve double files"

---
+++ python/break_and_continue.py
@@ -0,0 +1,14 @@
+print("The break instruction:")
+for i in range(1, 6):
+    if i == 3:
+        break
+    print("Inside the loop.", i)
+print("Outside the loop.")
+
+print("\n continue instruction:")
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print("Inside the loop.", i)
+print("Outside the loop.")
+    
