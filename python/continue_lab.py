a19c09f6bcb34f75f411db83c43ea45577bd7bbf
oktvsm <oktavianuss.misro@gmail.com>
2022-10-12 21:24:42 +0700

    submit solution for continue_lab.py

---
+++ python/continue_lab.py
@@ -0,0 +1,15 @@
+word_entered = input("Please enter a word: ")
+user_word = word_entered.upper()
+
+for letter in user_word:
+    if letter == "A":
+        continue
+    if letter == "I":
+        continue
+    if letter == "U":
+        continue
+    if letter == "E":
+        continue
+    if letter == "O":
+        continue
+    print(letter)
