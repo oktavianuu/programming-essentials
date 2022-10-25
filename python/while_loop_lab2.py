61c027ade58dda0580087e7e8e82b286c4d0a204
oktvsm <oktavianuss.misro@gmail.com>
2022-10-25 10:08:16 +0700

    add while loop lab 2 but not finished yet

---
+++ python/while_loop_lab2.py
@@ -0,0 +1,15 @@
+c0 = int(input("Enter non-negative and non-zero integer: "))
+
+if c0 > 1:
+    steps = 0
+    while c0 > 1:
+        if c0 % 2 != 0:
+            3 * c0 + 1
+        else:
+            cnew = c0 // 2
+        #
+        # code goes here 
+        #
+    print('steps =', steps)
+else:
+    print("Bad c0 value.")
