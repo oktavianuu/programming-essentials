f40b1824c36f789da1193a2a632006447fc67039
oktvsm <oktavianuss.misro@gmail.com>
2022-11-01 15:13:37 +0700

    slution for while_loop_lab2.py

--- python/while_loop_lab2.py
+++ python/while_loop_lab2.py
@@ -2,14 +2,14 @@ c0 = int(input("Enter non-negative and non-zero integer: "))
 
 if c0 > 1:
     steps = 0
-    while c0 > 1:
+    while c0 != 1:
         if c0 % 2 != 0:
-            3 * c0 + 1
+            cnew = 3 * c0 + 1
         else:
             cnew = c0 // 2
-        #
-        # code goes here 
-        #
+        print(c0)
+        c0 = cnew
+        steps =+ 1
     print('steps =', steps)
 else:
     print("Bad c0 value.")
