dd836497558b52a971d5635953f2b707d3540503
oktavianu <oktavianuss.misro@gmail.com>
2023-02-27 10:50:57 +0700

    second for continue

---
+++ python/for_continue2.py
@@ -0,0 +1,18 @@
+word_without_vowels = " "
+user_word = input("Enter a word: ")
+user_word = user_word.upper()
+
+for letter in user_word:
+    if letter == "A":
+        continue
+    if letter == "E":
+        continue
+    if letter == "I":
+        continue
+    if letter == "U":
+        continue
+    if letter == "O":
+        continue
+    word_without_vowels += letter
+
+print(word_without_vowels)
