b86a6976253b79408bfc56817ecbb97ef0929e5d
oktavianu <oktavianuss.misro@gmail.com>
2023-03-24 11:26:02 +0700

    add new file

---
+++ python/function_testing.py
@@ -0,0 +1,21 @@
+def is_leap_year(year):
+    if year % 4 != 0:
+        return False
+    elif year % 100 != 0:
+        return True
+    elif year % 400 != 0:
+        return False
+    else:
+        return True
+    
+test_data = [1900, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+
+for i in range(len(test_data)):
+    yr = test_data[i]
+    print(yr, "->", end="")
+    result = is_leap_year(yr)
+    if result == test_results[i]:
+        print("OK")
+    else:
+        print("Failed")
