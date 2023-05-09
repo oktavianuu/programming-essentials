9b0156364f5762b1eaadf04efaa09b4d9ee478d1
oktavianu <oktavianuss.misro@gmail.com>
2023-05-09 11:48:24 +0700

    update access dictionary

--- python/access_dictionary.py
+++ python/access_dictionary.py
@@ -4,8 +4,19 @@ dictionary = {
     "horse": "cheval"
     }
 
+# accessing dictionary with keys()
+print("keys() method")
 for key in dictionary.keys():
     print(key, "->", dictionary[key])
 
 # because dictionary isn't a seq type, we cannot just use for loop like list
 # but instead, we can use for loop with key to access the key and value of a dictionary
+
+# accessing dictionary with items()
+print("items() method")
+for english, french in dictionary.items():
+    print(english, "->", french)
+
+print("values() method")
+for french in dictionary.values():
+    print(french)
