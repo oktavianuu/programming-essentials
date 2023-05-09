edae5bea663869fc7c10fc6ad1a08bfde201dc9f
oktavianu <oktavianuss.misro@gmail.com>
2023-05-09 11:20:00 +0700

    dictionary

---
+++ python/dictionary.py
@@ -0,0 +1,10 @@
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+
+# checking words in dictionary by check its key
+words = ['cat', 'dog', 'horse', 'lion']
+
+for word in words:
+    if word in dictionary:
+        print(word, "->", dictionary[word])
+    else:
+        print(word, "isn't present in dictionary!")
