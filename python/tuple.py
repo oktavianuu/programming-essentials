5f017f968fd648546f63079f0fa5bb2575cd3618
oktavianu <oktavianuss.misro@gmail.com>
2023-05-10 15:55:39 +0700

    add tuple

---
+++ python/tuple.py
@@ -0,0 +1,20 @@
+# loop through tuple
+tuple_1 = (1, 2, 3)
+for elem in tuple_1:
+    print(elem)
+
+# check if element present or not
+tuple_2 = (1, 2, 3, 4)
+print(5 in tuple_2)
+print(5 not in tuple_2)
+
+# check the length of tuple
+tuple_3 = (1, 2, 3, 5)
+print(len(tuple_3))
+
+# multiply and add
+tuple_4 = tuple_1 + tuple_2
+tuple_5 = tuple_3 * 2
+
+print(tuple_4)
+print(tuple_5)
