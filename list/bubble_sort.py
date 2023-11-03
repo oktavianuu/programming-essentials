f6e04f78e60b4736c351011f87db5b3efbf0a708
oktavianuu <oktavianuss.misro@gmail.com>
2023-11-03 09:44:23 +0700

    update things

---
+++ list/bubble_sort.py
@@ -0,0 +1,4 @@
+# There are many sorting algorithm to sort elements of a list. Different algorithms different speed and effectiveness. 
+# One of them are bubble sort. 
+
+# Let's assume that list can be sorted in two ways
