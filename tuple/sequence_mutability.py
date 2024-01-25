22f8848baf2d7f74f03cd71d6f3b6e74e237dff6
oktavianuu <oktavianuss.misro@gmail.com>
2024-01-25 12:13:17 +0700

    tuple

---
+++ tuple/sequence_mutability.py
@@ -0,0 +1,8 @@
+# Sequence type is data type in python which is able to store more than one value (or less than one, as a sequence may be empty), 
+# and this values can be sequentially (hence the name) browsed, element by element.
+# for loop is a tool specifically designed to iterate over sequence, in other words, sequence is data which can be scanned by the for loop.
+# mutability is a property of any python's data that describes it's readiness to be freely changed during program execution.
+# Two kinds of python's data based on its mutability: mutable and immutable.
+# mutable data can be freely updated at any times meanwhile, immutable data is the opposite.
+
+
---
+++ tuple/tuple.md
@@ -0,0 +1,17 @@
+### What is Tuple?
+look at the following code:
+```python
+tuple_1 = (1, 2, 4, 5)
+tuple_2 = (1., .5, .25, .125)
+```
+The above example showed us two tuples containing four elements. If we print them, this is what we see on our console:
+```
+(1, 2, 4, 5)
+(1., .5, .25, .125)
+```
+#### How to create tuple?
+This is how:
+```python
+empty_tuple = ()
+```
+When we create tuple, we need parenthesis. It is important to remember that forgetting commas won't spoil the program, instead, we will get two single variables, not tuple.
