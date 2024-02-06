43a3d19c1fe194926fec4f74603a082962d106ac
oktavianuu <oktavianuss.misro@gmail.com>
2024-02-06 14:54:37 +0700

    update things

--- dictionary/tup_dict.py
+++ dictionary/tup_dict.py
@@ -1,17 +1,17 @@
 school_class = {} # initialize empty dict to store the data
 
-while True:
-    name = input("Enter the student's name: ")
-    if name == "":
-        break
+while True: # infinite loop as long as the condition is true
+    name = input("Enter the student's name: ") # read the student's name
+    if name == "": #if the name is empty
+        break       # leave the loop
 
-    score = int(input("Enter the student's score (0-10): "))
-    if score not in range(0, 11):
-        break
+    score = int(input("Enter the student's score (0-10): ")) # ask for one of the student's score (integer range 0-10)
+    if score not in range(0, 11): # if the score is not in range 0-10
+        break # leave the loop
 
-    if name in school_class:
-        school_class[name] += (score, )
-    else:
+    if name in school_class: # if the name of the student is already in the dictionary
+        school_class[name] += (score, ) # lengthen the associated tuple with the new score (the "+=" operator)
+    else:                   # if this is a new student (unknown to the dict), create a new entry
         school_class[name] = (score, )
     
 for name in sorted(school_class.keys()):
